236fff45111572e1b91da5412c94b25c51307e0e
pranjitxkalita01 <pranjitxkalita@gmail.com>
2026-08-14 23:10:42 +0530

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = "my-music-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js"
+];
+
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+                .then(cache =>
+                    cache.addAll(
+                        FILES_TO_CACHE
+                    )
+                )
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+            self.clients.claim()
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(event.request)
+                .then(cachedResponse => {
+
+                    return (
+                        cachedResponse ||
+                        fetch(event.request)
+                    );
+
+                })
+
+        );
+
+    }
+);
